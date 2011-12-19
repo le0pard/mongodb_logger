bd941d27700814906811f2f64a598299e67cdd24
Alexey <leopard.not.a@gmail.com>
2011-12-20 00:19:52 +0200

    add steps for web

--- features/mongodb_logger_web.feature
+++ features/mongodb_logger_web.feature
@@ -3,3 +3,12 @@ Feature: Install MongodbLogger Web and test it
   Scenario: Main page
     Given homepage
     Then I should see text that no logs in system
+    
+  Scenario: Tail logs buttons
+    Given homepage
+    And I should see start tail button
+    When I click on start tail button
+    Then I should see stop tails button
+    And box with time of last log tail
+    When I click on stop tail button
+    Then I should see start tail button
--- features/step_definitions/mongodb_logger_web_steps.rb
+++ features/step_definitions/mongodb_logger_web_steps.rb
@@ -20,3 +20,26 @@ end
 Then /^I should see text that no logs in system$/ do
   page.has_selector?('div', :text => 'No logs found, try to filter out the other parameters', :visible => true)
 end
+
+Given /^I should see start tail button$/ do
+  page.has_link?('tail_logs_link', :visible => true)
+  page.has_link?('tail_logs_stop_link', :visible => false)
+end
+
+When /^I click on start tail button$/ do
+  click_link('tail_logs_link')
+end
+
+Then /^I should see stop tails button$/ do
+  page.has_link?('tail_logs_link', :visible => false)
+  page.has_link?('tail_logs_stop_link', :visible => true)
+end
+
+Then /^box with time of last log tail$/ do
+  page.has_selector?('span', :id => 'tail_logs_time', :visible => true)
+end
+
+When /^I click on stop tail button$/ do
+  click_link('tail_logs_stop_link')
+end
+
