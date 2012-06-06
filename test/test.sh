35c91106acf87000ad74f5c2cbf27431ce34ee11
Alexey <leopard.not.a@gmail.com>
2012-06-06 22:49:59 +0300

    fix testing and rails versions

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rake vendor_test_gems && \
+BUNDLE_GEMFILE=test/Gemfile_tests bundle
+BUNDLE_GEMFILE=test/Gemfile_tests rake vendor_test_gems && \
 rake test && \
 rake cucumber:web && \
 #rake jasmine:ci && \
