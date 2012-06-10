c767774292f0c10cb5a3a2193fa066f140fabd2e
Alexey <leopard.not.a@gmail.com>
2012-06-10 15:48:55 +0300

    capistrano recipe

---
+++ lib/mongodb_logger/capistrano.rb
@@ -0,0 +1,17 @@
+Capistrano::Configuration.instance.load do
+  after 'deploy:update_code', 'deploy:assets:precompile'
+  _cset :mongodb_logger_asset_env, "RAILS_GROUPS=assets"
+  _cset :mongodb_logger_assets_role, [:web]
+  _cset :mongodb_logger_assets_dir, "public/assets"
+
+  namespace :mongodb_logger do
+
+    desc <<-DESC
+      Run the asset precompilation rake task. 
+    DESC
+    task :precompile, :roles => mongodb_logger_assets_role, :except => { :no_release => true } do
+      run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{mongodb_logger_asset_env} rake mongodb_logger:assets:compile[#{mongodb_logger_assets_dir}]"
+    end
+
+  end
+end
