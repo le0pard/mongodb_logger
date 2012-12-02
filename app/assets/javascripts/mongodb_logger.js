3f862128af487968162815b6e2fd85132ba10614
Alexey <leopard.not.a@gmail.com>
2012-12-02 14:07:06 +0200

    fix jquery highlight

--- app/assets/javascripts/mongodb_logger.js
+++ app/assets/javascripts/mongodb_logger.js
@@ -1,2 +1,6 @@
-//= require_tree ./vendors/
+//= require ./vendors/jquery-1.8.3.min.js
+//= require ./vendors/jquery-ui-1.9.2.min.js
+//= require ./vendors/jquery.pjax.min.js
+//= require ./vendors/highlight.pack.js
+
 //= require logs.js.coffee
