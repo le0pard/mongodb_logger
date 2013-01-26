d4b1b1996af3ce406729f8424111a0cf31d92c01
sergey_mo <svmostovoy@gmail.com>
2013-01-26 16:42:47 +0200

    add rack middleware

---
+++ lib/mongodb_logger/rack_middleware.rb
@@ -0,0 +1,24 @@
+#Rack middleware for mounted rack app (e.g Grape)
+module MongodbLogger
+  class RackMiddleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      request = ::Rack::Request.new env
+      path = request.path.split('/')
+      log_attrs = {:method    => request.request_method,
+                :action     => path[2..-1].join('/'),
+                :controller => path[1],
+                :path       => request.path,
+                :url        => request.url,
+                :params     => request.params,
+                :ip         => env['REMOTE_ADDR']  }
+
+      Rails.logger.mongoize(log_attrs) do
+        return @app.call(env)
+      end
+    end
+  end
+end
