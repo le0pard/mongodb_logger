620c06aeaff5481347f852e2d07d177bddea1de0
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-03-14 00:36:28 +0200

    begin new steps

--- features/step_definitions/rails_application_steps.rb
+++ features/step_definitions/rails_application_steps.rb
@@ -1,13 +1,11 @@
 require 'active_support/core_ext/string/inflections'
 
-Then(/^I should generate in public\/assets files$/) do
+Then(/^I should generate in assets folder mongodb_logger files$/) do
   js_and_css = false
   Dir["#{LOCAL_RAILS_ROOT}/public/assets/*{.js,.css}"].each do |file|
+    puts File.basename(file).inspect
     js_and_css = true unless /^mongodb_logger\-([0-9a-z]+)\.(js|css)$/.match(File.basename(file)).nil?
   end
-  raise "mongodb_logger gem not installed in rails" if false == js_and_css
+  raise "mongodb_logger gem not installed in rails (assets not compiled)" if false == js_and_css
 end
 
-Then /^I should see the Rails version$/ do
-  step %{I should see "Rails: #{ENV["RAILS_VERSION"]}"}
-end
