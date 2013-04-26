6536cf6be450654436d14dcbd42a971eee29edde
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-04-26 17:49:12 +0300

    rails logger

--- lib/mongodb_logger/rails_logger.rb
+++ lib/mongodb_logger/rails_logger.rb
@@ -1,8 +1,4 @@
+require 'active_support/core_ext/logger' unless defined?(ActiveSupport::Logger)
 module MongodbLogger
-  if defined?(ActiveSupport::Logger)
-    class RailsLogger < ActiveSupport::Logger; end
-  else
-    require 'active_support/core_ext/logger'
-    class RailsLogger < ActiveSupport::BufferedLogger; end
-  end
+  class RailsLogger < (defined?(ActiveSupport::Logger) ? ActiveSupport::Logger : ActiveSupport::BufferedLogger); end
 end
