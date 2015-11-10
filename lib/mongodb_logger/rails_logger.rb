db123da904e5dbe77af76fe228e01890cb040d54
Krzysztof Buszewicz <kbuszewicz@grupainteger.pl>
2015-11-10 17:16:54 +0100

    do not affect RailsLogger if it inherits from ActiveSupport::BufferedLogger

--- lib/mongodb_logger/rails_logger.rb
+++ lib/mongodb_logger/rails_logger.rb
@@ -1,16 +1,14 @@
 module MongodbLogger
   if defined?(ActiveSupport::Logger)
-    class RailsLogger < ActiveSupport::Logger; end
+    class RailsLogger < ActiveSupport::Logger
+      def initialize(path, level)
+        super(path)
+        @level = level
+        @logdev.instance_variable_set(:@shift_age, nil)
+        @logdev.instance_variable_set(:@shift_size, nil)
+      end
+    end
   else
     class RailsLogger < ActiveSupport::BufferedLogger; end
-  end
-
-  RailsLogger.class_eval do
-    def initialize(path, level)
-      super(path)
-      @level = level
-      @logdev.instance_variable_set(:@shift_age, nil)
-      @logdev.instance_variable_set(:@shift_size, nil)
-    end
   end
 end
