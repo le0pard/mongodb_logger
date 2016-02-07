4b1ffb64b99ea6386c3b74a2d44b1c7d68944148
Anton Holovko <anton.holovko.b@gmail.com>
2016-02-07 13:08:19 +0200

    fix file path for capistrano 2

--- lib/mongodb_logger/capistrano.rb
+++ lib/mongodb_logger/capistrano.rb
@@ -1,5 +1,5 @@
 if Gem::Specification.find_by_name('capistrano').version >= Gem::Version.new('3.0.0')
-  load File.expand_path('../capistrano/tasks/capistrano3.rake', __FILE__)
+  load File.expand_path('../../capistrano/tasks/capistrano3.rake', __FILE__)
 else
-  require_relative '../capistrano/tasks/capistrano3.rake'
+  require File.expand_path('../../capistrano/tasks/capistrano2.rb', __FILE__)
 end
